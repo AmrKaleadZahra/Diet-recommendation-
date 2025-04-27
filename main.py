192d3cc48ce863d276e9b40e6925d7e95d788424
AmrKaleadZahra <111576553+AmrKaleadZahra@users.noreply.github.com>
2025-04-27 20:12:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ import os
 
 
 # === إعداد Google Drive ===
-file_id = '1GbpAOBR_A2WqLZVMp180oty-Pf_Jz-CE'  # ضع هنا ID الخاص بملفك
+file_id = '1WW1XU9TTXE8BWb0Nr1IcfyMOqUlzuj5B'  # ضع هنا ID الخاص بملفك
 url = f"https://drive.google.com/uc?id={file_id}"
-local_filename = "recipes_with_prices4.csv"
+local_filename = "recipes_with_prices5.csv"
 
 # تحميل الملف لو مش موجود
 if not os.path.exists(local_filename):
